a290dd441bda5081a4d14896395f140f71727a84
Victor <victor.almeida52@etec.sp.gov.br>
2023-08-17 21:25:16 -0300

    basic structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-  return <h1>Hello World</h1>;
-}
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
-export default App;
+import "./global.css";
+
+import styles from "./App.module.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Victor Almeida"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, minus? Delectus voluptas eveniet sunt sit voluptates ipsum corporis rem tempore quis? Ullam quisquam voluptate porro consectetur. Numquam, eos! Ipsam, earum!"
+          />
+
+          <Post author="Diego Fernandes" content="Um novo post muito legal!" />
+        </main>
+      </div>
+    </div>
+  );
+}
